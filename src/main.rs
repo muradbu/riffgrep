079fb2b08b040766aa09886c5bab931ebc3736e3
muradbuyukasik <10285764+muradbuyukasik@users.noreply.github.com>
2022-06-06 17:21:17 +0200

    cli args practice

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+#![allow(unused)]
+
+use clap::Parser;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+    /// The pattern to look for
+    song_title: String,
+    // The path to the file to read
+    // #[clap(parse(from_os_str))]
+    // path: std::path::PathBuf,
+}
+
+impl std::fmt::Display for Cli {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(
+            f,
+            "(song: {})",
+            self.song_title,
+            // self.path.display()
+        )
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    println!("{}", args);
 }
